afe3e43aff72993c62cbef74cfbfb1ccca25d0c1
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2022-12-29 12:58:33 +0100

    MQ i obsługa nav mobile hamburger

---
+++ script.js
@@ -0,0 +1,12 @@
+// NAV MOBILE HAMBURGER
+const HAMBURGER = document.querySelector(".hamburger");
+const NAV_MOBILE = document.querySelector(".nav-top-mobile");
+const CLOSE_BTN = document.querySelector("#close");
+
+HAMBURGER.addEventListener("click", () => {
+    NAV_MOBILE.style.left = "0";
+});
+
+CLOSE_BTN.addEventListener("click", () => {
+    NAV_MOBILE.style.left = "-4000px";
+})
